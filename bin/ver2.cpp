b26737fb4ff5cd585e33deb8011477f9e4b685b4
AdamPas <adaDefier@gmail.com>
2020-06-04 16:55:42 +0200

    Slight change in the garden

--- bin/ver2.cpp
+++ bin/ver2.cpp
@@ -2,6 +2,13 @@
 // Created by adam on 24.05.20.
 //
 
+/*
+TODO: 1) Check the classes implementation, play a bit with classes and inheritance
+      2) Change properties to HP and stuff
+      3) Have plants specializations -> warrior, defender, caretaker
+      4) Log stuff in a file
+ */
+
 #include "Garden/Plant.hpp"
 #include "Garden/Garden.hpp"
 
